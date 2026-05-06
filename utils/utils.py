bb48daaa94b947edac46e498dec741911cf98edc
Daehun_Bae <76238341+qoeogns09@users.noreply.github.com>
2026-05-06 15:19:26 +0900

    add genome feature from file

--- utils/utils.py
+++ utils/utils.py
@@ -7,6 +7,7 @@ from scipy.stats import kendalltau
 import numpy as np
 import torch
 import torch.nn as nn
+from Bio import SeqIO
 
 def set_random_seed(random_seed = 42):
     torch.manual_seed(random_seed)
@@ -34,3 +35,62 @@ def compute_metrics(preds, labels):
         'Pearson Q' : pearson,
         # 'Kendall tau' : tau,
     }
+       
+def count_mer(mer, seq):
+    mer_count = {}
+    for oligo in mer:
+        mer_count[oligo] = seq.count(oligo)
+    return mer_count    
+
+def normalize(x):
+    norm = np.linalg.norm(x)
+    return x/norm
+
+def get_features(seqs):
+    features1 = 0
+    features2 = 0
+    features3 = 0
+    features4 = 0
+    for seq in seqs:
+        nucleotide = ['A', 'T', 'G', 'C']
+        mer_1 = nucleotide
+        mer_2 = []
+        for i in nucleotide:
+            for j in nucleotide:
+                mer_2.append(i+j)
+        mer_3 = []
+        for i in nucleotide:
+            for j in nucleotide:
+                for k in nucleotide:
+                    mer_3.append(i+j+k)
+        mer_4 = []
+        for i in nucleotide:
+            for j in nucleotide:
+                for k in nucleotide:
+                    for l in nucleotide:
+                        mer_4.append(i+j+k+l)
+
+        count_1 = count_mer(mer_1, seq)
+        count_2 = count_mer(mer_2, seq)
+        count_3 = count_mer(mer_3, seq)
+        count_4 = count_mer(mer_4, seq)
+
+        features1 += np.array(list(count_1.values()))
+        features2 += np.array(list(count_2.values()))
+        features3 += np.array(list(count_3.values()))
+        features4 += np.array(list(count_4.values()))
+    features1 = normalize(features1)
+    features2 = normalize(features2)
+    features3 = normalize(features3)
+    features4 = normalize(features4)
+    
+    return np.concatenate((features1, features2, features3, features4))
+
+def get_genome_feature_from_file(file_path):
+    genome_seqs = []
+    descriptions = []
+    for seq_record in SeqIO.parse(file_path, 'fasta'):
+        descriptions.append(seq_record.description)
+        genome_seqs.append(seq_record.seq)
+
+    return get_features(genome_seqs)
